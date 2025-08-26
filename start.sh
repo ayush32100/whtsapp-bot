ec70ae8dbdaa02af297df604637b9d1dbd6b1c1a
ayush32100 <128618342+ayush32100@users.noreply.github.com>
2025-08-26 17:02:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# Make sure script is executable: chmod +x start.sh
-python3 whatsapp_ai_bot.py
+
+# Install Chromium + ChromeDriver
+sudo apt-get update
+sudo apt-get install -y chromium-browser chromium-chromedriver
+
+# Run the bot
+python3 whatsapp_sambanova_bot_headless.py
